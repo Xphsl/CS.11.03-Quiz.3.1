f351f78dca1876488e1a2eebb00f3918e8d7481a
Xphsl <stevenl2026@student.cis.edu.hk>
2023-10-06 09:42:11 +0800

    Quiz 3.1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,55 @@
 public class Main {
 
     // Question 1 - calculateGrade
+    public static int calculateGrade(int num) {
+        if (num >= 90) {
+            return "A";
+        }
+        if (num >= 80 && num <= 89) {
+            return "B";
+        }
+        if (num >= 70 && num <= 79) {
+            return "C";
+        }
+        if (num >= 60 && num <= 69) {
+            return "D";
+        }
+        if (num >= 50 && num <= 59) {
+            return "E";
+        }
+        if (num <= 49) {
+            return "F";
+        }
+    }
 
     // Question 2 - fizzBuzz
+    public static int fizzBuzz(int numb) {
+    }
+
+    if(numb%3==0)
+
+    {
+        return "Fizz";
+    }
+    if(numb%5==0)
+
+    {
+        return "Buzz";
+    }
+}
 
     // Question 3 - frontBack
+public static String frontBack(String he) {
+    if (he)
 
     // Question 4 - twoAsOne
-
+public static Boolean( int a, int b, int c)
+    if ((a + b == c) || (b + c == a) || (a + c == b)) {
+        return true;
+    }
+    return false;
+}
     // Question 5 - endUp
-
+    public static String(String a){
+    if ()
 }
